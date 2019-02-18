199748a43cb29e07a425fda9fe2bd42bc233549e
Sushil Mittal <sushilmittal1992@gmail.com>
2019-02-18 10:31:11 +0530

    initial commit

---
+++ src/main/java/com/ankuran/wages/WagesApplication.java
@@ -0,0 +1,13 @@
+package com.ankuran.wages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagesApplication.class, args);
+	}
+
+}
